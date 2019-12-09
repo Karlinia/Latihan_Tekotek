23dfbdeb0167faa5659fe0594a64e3e27d04966d
Karlinia <Niakarlinia9991@gmail.com>
2019-12-10 03:37:18 +0700

    Tekotek

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Anak Ayam</title>
+	<style>
+		.ganjil{
+			color: orange;
+		}
+	</style>
+</head>
+<body>
+	<p class="ganjil">Teks Oranye</p>
+	<?php
+	for ($anak_ayam=8; $anak_ayam <=8; anak_ayam--){
+		echo "tekotekoktekotek $anak_ayam";
+	}
+
+?>
+
+</body>
+</html>
